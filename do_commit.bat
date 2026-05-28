a89508d1682f8ab0b5e35df52eaaed48fe139d5c
CYBERDUDEBIVASH <bivash@cyberdudebivash.com>
2026-05-28 13:48:29 +0530

    chore: remove legacy bat scripts + harden .gitignore

--- do_commit.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d C:\Users\Administrator\Desktop\cyberdudebivash-blog
-git commit -m "GOD LEVEL v4: Fix zero-value fields, static fallback, ecosystem footer, monetization"
-git push origin main
-echo DONE
--- extract_v10.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d C:\Users\Administrator\Desktop\cyberdudebivash-blog
-git show 4fc14bf:index.html > index_v10_clean.html
-echo DONE > extract_done.txt
-git log --oneline -1 >> extract_done.txt
